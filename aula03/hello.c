b7fbe7440ca3c53467d17f71f9c9d9dd8eb2a025
Gabriel Leoncio Verdi <101580365+glVerdi@users.noreply.github.com>
2025-03-06 13:53:52 +0000

    adicionando mais comentarios do gcc

--- aula03/hello.c
+++ aula03/hello.c
@@ -6,5 +6,6 @@ int main (void)
   return 0;
 }
 
-// ggc -E hello.c > hello.pre-processamento - gera o pré-processamento (pré-processamento)
-// gcc -S hello.c - gera o assembler (compilação)
+// ggc -E hello.c > hello.pre-processamento - gera o pré-processamento (pré-processamento), está no hello.pre-processamento
+// gcc -S hello.c - gera o assembler (compilação), está no hello.s
+// gcc -c hello.c - não faz ligações com nenhuma biblioteca, está no hello.c
