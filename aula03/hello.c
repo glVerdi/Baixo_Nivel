40b6ba0ad039e8eecad74eaecac94387fc45ddec
Gabriel Leoncio Verdi <101580365+glVerdi@users.noreply.github.com>
2025-03-06 13:49:06 +0000

    adicionando comentarios sobre o gcc

--- aula03/hello.c
+++ aula03/hello.c
@@ -5,3 +5,6 @@ int main (void)
   printf("Hello World!\n");
   return 0;
 }
+
+// ggc -E hello.c > hello.pre-processamento - gera o pré-processamento (pré-processamento)
+// gcc -S hello.c - gera o assembler (compilação)
